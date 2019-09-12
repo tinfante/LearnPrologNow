881ecd5e2f2bb90e7ccfdae723f0c7a45f251524
Tomás Infante <quete@importa.com>
2019-09-11 22:24:02 -0300

    adds ch3 ex3.4

--- chapter3/exercises.pl
+++ chapter3/exercises.pl
@@ -59,3 +59,29 @@ directTrain(nancy,metz).
 travelFromTo(A, C) :- directTrain(A, C).
 travelFromTo(A, C) :- directTrain(A, B),
                       travelFromTo(B, C).
+
+
+% Exercise 3.4. Define a predicate greater_than/2 that takes two numerals
+%in the notation that we introduced in this lecture (i.e. 0, succ(0),
+% succ(succ(0)) ...) as arguments and decides whether the first one is
+% greater than the second one. E.g:
+% ?- greater_than(succ(succ(succ(0))),succ(0)).
+% yes
+% ?- greater_than(succ(succ(0)),succ(succ(succ(0)))).
+% no
+
+greater_than(succ(_), 0).
+greater_than(succ(X), succ(Y)) :-
+  greater_than(X, Y).
+
+% How I think this works. The first rule is the base case, and matches
+% succ(_), where _ is the disposable variable, as the first argument and
+% 0 as the second argument. In that case it evaluates to true, the first
+% argument is greater than the second one. If there is no match for the
+% first rule, the second rule is attempted. It matches succ(X) and succ(Y)
+% assigning whats inside succ() to X and Y respectively, and then tries to
+% resolve X and Y again. Eventually either X or Y will be stripped of all
+% their wrapping succ(). If X still has at least 1 succ() and Y is 0, we
+% know that X is greater. So:
+%       ?- greater_than(succ(succ(qwe(99))), succ(0)).
+%       true.
