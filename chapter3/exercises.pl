9c8cb6c25e69d3cc1f19ddb0dc4b3afae0347f3c
Tomás Infante <quete@importa.com>
2019-09-14 19:40:46 -0300

    adds ch3 ex5

--- chapter3/exercises.pl
+++ chapter3/exercises.pl
@@ -85,3 +85,26 @@ greater_than(succ(X), succ(Y)) :-
 % know that X is greater. So:
 %       ?- greater_than(succ(succ(qwe(99))), succ(0)).
 %       true.
+
+
+% Exercise 3.5 Binary trees are trees where all internal nodes have exactly
+% two children. The smallest binary trees consist of only one leaf node. We
+% will represent leaf nodes as leaf(Label) . For instance, leaf(3) and leaf(7)
+% are leaf nodes, and therefore small binary trees. Given two binary trees
+% B1 and B2 we can combine them into one binary tree using the functor tree/2
+% as follows: tree(B1,B2) . So, from the leaves leaf(1) and leaf(2) we can
+% build the binary tree tree(leaf(1),leaf(2)). And from the binary trees
+% tree(leaf(1),leaf(2)) and leaf(4) we can build the binary tree
+% tree(tree(leaf(1),  leaf(2)),leaf(4)).
+
+% Now, define a predicate swap/2, which produces the mirror image of the
+% binary tree that is its first argument. For example:
+%    ?-  swap(tree(tree(leaf(1),  leaf(2)),  leaf(4)),T).
+%    T  =  tree(leaf(4),  tree(leaf(2),  leaf(1))).
+%    yes
+
+swap(leaf(X), leaf(X)).
+
+swap(tree(X, Y), tree(Xm, Ym)) :-
+  swap(X, Ym),
+  swap(Xm, Y).
